9fdaa9f814f0d7facdbf92d78bb6fee799f2160e
drwiner <drwiner131@gmail.com>
2017-01-19 10:15:58 -0700

    small editing error

--- Viterbi.py
+++ Viterbi.py
@@ -13,7 +13,7 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
 	V = [{}]
 	for st in states:
 		V[0][st] = {"prob": start_p[st] * emit_p[st][obs[0]], "prev": None}
-		# Run Viterbi when t > 0
+	# Run Viterbi when t > 0
 	for t in range(1, len(obs)):
 		V.append({})
 		for st in states:
@@ -25,21 +25,21 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
 					break
 	for line in dptable(V):
 		print(line)
-		opt = []
-		# The highest probability
-		max_prob = max(value["prob"] for value in V[-1].values())
-		previous = None
-		# Get most probable state and its backtrack
-		for st, data in V[-1].items():
-			if data["prob"] == max_prob:
-				opt.append(st)
-				previous = st
-				break
-		# Follow the backtrack till the first observation
-		for t in range(len(V) - 2, -1, -1):
-			opt.insert(0, V[t + 1][previous]["prev"])
-			previous = V[t + 1][previous]["prev"]
-		print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+	opt = []
+	# The highest probability
+	max_prob = max(value["prob"] for value in V[-1].values())
+	previous = None
+	# Get most probable state and its backtrack
+	for st, data in V[-1].items():
+		if data["prob"] == max_prob:
+			opt.append(st)
+			previous = st
+			break
+	# Follow the backtrack till the first observation
+	for t in range(len(V) - 2, -1, -1):
+		opt.insert(0, V[t + 1][previous]["prev"])
+		previous = V[t + 1][previous]["prev"]
+	print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
 
 def dptable(V):
 	# Print a table of steps from dictionary

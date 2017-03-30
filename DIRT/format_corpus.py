ee6a6f9c5e0ea7d68a83eec8f83928f0c40975fc
drwiner <drwiner131@gmail.com>
2017-03-30 17:27:36 -0600

    format_corpus seems complete. output DIRT readable corpus

--- DIRT/format_corpus.py
+++ DIRT/format_corpus.py
@@ -17,11 +17,57 @@ def sent_to_doc(sent):
 		raw += token.orth_ + ' '
 	return nlp(raw)
 
+def noun_chunk_index_list(nchunks):
+	indices = []
+	index_chunk_dict = dict()
+	for nchunk in nchunks:
+		indices.extend(range(nchunk.start, nchunk.end))
+		index_chunk_dict[nchunk.end] = nchunk #.root
+	return indices, index_chunk_dict
+
+symb_dict = dict()
+symb_dict['\"'] = '>SQUOTE'
+symb_dict['\''] = '>SQUOTE'
+symb_dict['&'] = '>AMPERSAND'
+symb_dict[','] = '>COMMA'
+symb_dict['('] = '>LPAREN'
+symb_dict[')'] = '>RPAREN'
+symb_dict['-'] = '>MINUS'
+symb_dict['--'] = '>MINUS'
+symb_dict['.'] = '>PERIOD'
+
+
 def parse_sent(sent):
+	parse_list = []
 	s_doc = sent_to_doc(sent)
-	for i, token in sent:
+	nchunks = list(s_doc.noun_chunks)
+	nchunk_indices, nchunk_dict = noun_chunk_index_list(nchunks)
+	for i, token in enumerate(sent):
 		# if the index is a noun chunk, ignore, we will just insert all words between them? keep an origi
-		pass
+		if i in nchunk_indices:
+			if i in nchunk_dict.keys():
+				parse_list.append(nchunk_dict[i])
+		else:
+			if token.orth_ in symb_dict.keys():
+				parse_list.append(symb_dict[token.orth_])
+			else:
+				parse_list.append(token)
+	parse_list.append('<EOS')
+	return parse_list
+
+
+def parse_to_output(sents, file_name):
+	# open the file name
+	with open(file_name, 'w') as fn:
+		for snt in sents:
+			for elm in parse_sent(snt):
+				if type(elm) is spacy.tokens.span.Span:
+					fn.write('{} : {}'.format('NP', elm.orth_))
+				elif type(elm) is str:
+					fn.write('WORD : {}'.format(elm))
+				else:
+					fn.write('{} : {}'.format(elm.pos_, elm.orth_))
+				fn.write('\n')
 
 
 def get_sentences(docu):
@@ -31,9 +77,11 @@ def get_sentences(docu):
 		sents.append([doc[i] for i in range(span.start, span.end)])
 	return sents
 
+
 if __name__ == '__main__':
 	file_name = 'combo.txt'
 	print('formatting corpus: {}'.format(file_name))
 	doc = read_corpus(file_name)
 	snts = get_sentences(doc)
-	print('check ehre')
+	output_name = 'movie_corpus.txt'
+	parse_to_output(snts, output_name)
